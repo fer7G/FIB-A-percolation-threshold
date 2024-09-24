cb76594ebac75700006e50555456696e0fd689ed
Alvaro Monclus Muñoz <alvarom@MacBook-Air-de-Alvaro.local>
2024-09-24 17:37:51 +0200

    Añadido herramiena de creacion de grafos

---
+++ graph_generator.py
@@ -0,0 +1,14 @@
+import networkx as nx
+
+def generate_random_graph(num_nodes, edge_prob, output_file):
+    G = nx.erdos_renyi_graph(num_nodes, edge_prob)
+
+    num_edges = G.number_of_edges()
+
+    with open(output_file, "w") as f:
+        f.write(f"p edge {num_nodes} {num_edges}\n")
+
+        for u, v in G.edges():
+            f.write(f"e {u + 1} {v + 1}\n")
+
+generate_random_graph(1000, 0.01, "test.dimacs")

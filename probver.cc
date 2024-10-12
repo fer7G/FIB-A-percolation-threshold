611089343156c9f8a4cefbb79bcb07ada577cd2d
davidc91x <davidcastropaniello@gmail.com>
2024-10-12 13:40:19 +0200

    Add files via upload
    Archivo de percolacion por vértices con el nuevo enfoque

---
+++ probver.cc
@@ -0,0 +1,60 @@
+#include "graph.cpp"
+#include <random>
+using namespace std;
+
+vector <float> calcularprobvertex(Graph a){
+    vector <float> ProbVertex (a.numNodes);
+    random_device generador0;
+    mt19937 generador(generador0());
+    uniform_real_distribution<> distribucion(0.0,1.0);
+    for(int i=0;i<a.numNodes;i++) {
+        ProbVertex[i]=distribucion(generador);
+
+    }
+     return ProbVertex;
+}
+
+int percolaciovertex(vector<int> ProbVertex, Graph a, float p){
+     vector <bool> nodosrestantes(a.numNodes,false);
+     int n=a.numNodes;
+     int nf=n;
+    for(int i=0; i<n; ++i) {
+        if(ProbVertex[i]<p){
+            nodosrestantes[i]=false;
+            nf--;
+
+        }
+
+    }
+
+    vector<int> nodosfinales(n);
+    int temp=nf-1;
+    for(int i=n-1;i>=0;--i){
+        if(nodosrestantes[i]){
+            nodosfinales[i]=temp;
+            --temp;
+        }
+    }
+    Graph inducido;
+    inducido.numNodes=nf;
+   // Matrix adjPercv;
+
+    for(int i=0; i<n; ++i) {
+        if(nodosrestantes[i]){
+            int n2=a.adjList[i].size();
+            for(int j=0; j<n2 ; ++j){
+                int conexion=a.adjList[i][j];
+                if( nodosrestantes[conexion]) {
+                    inducido.add_edge(nodosfinales[i],nodosfinales[conexion] );
+                  //  cout<<nodosfinales[i]<<" "<<nodosfinales[conexion]<<endl;
+                }
+
+            }
+        }
+
+    }
+    return inducido.connected_components();
+
+
+    }
+

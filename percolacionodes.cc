61ba234336e435fd7fcd44d704e093b7e34a74ad
davidc91x <davidcastropaniello@gmail.com>
2024-10-12 22:42:12 +0200

    Update percolacionodes.cc

--- percolacionodes.cc
+++ percolacionodes.cc
@@ -1,27 +1,25 @@
-#include <iostream>
+#include "graph.cpp"
 #include <random>
-#include <vector>
 using namespace std;
 
-int main(){
-    int n,m;
-    cin>>n>>m;
-    int nf=n;
-    vector < vector < int > > aristas(n);
-    vector <bool> nodosrestantes (n,true);
-    for(int i =0; i<m; i++){
-        int a,b;
-        cin>>a>>b;
-        aristas[a].push_back(b);
-    }
-    
-    float q;
-    cin>>q;
+vector <float> calcularprobvertex(const Graph& a){
+    vector <float> ProbVertex (a.numNodes);
     random_device generador0;
     mt19937 generador(generador0());
-    bernoulli_distribution distribucion(1-q);
+    uniform_real_distribution<> distribucion(0.0,1.0);
+    for(int i=0;i<a.numNodes;i++) {
+        ProbVertex[i]=distribucion(generador);
+
+    }
+     return ProbVertex;
+}
+
+int percolaciovertex(vector<int> ProbVertex,const Graph& a, float p){
+     vector <bool> nodosrestantes(a.numNodes,false);
+     int n=a.numNodes;
+     int nf=n;
     for(int i=0; i<n; ++i) {
-        if(distribucion(generador)){
+        if(ProbVertex[i]<p){
             nodosrestantes[i]=false;
             nf--;
 
@@ -29,7 +27,7 @@ int main(){
 
     }
 
-    vector<int> nodosfinales(nf);
+    vector<int> nodosfinales(n,-1);
     int temp=nf-1;
     for(int i=n-1;i>=0;--i){
         if(nodosrestantes[i]){
@@ -37,32 +35,25 @@ int main(){
             --temp;
         }
     }
-    m=0;
+    Graph inducido;
+    inducido.numNodes=nf;
+   // Matrix adjPercv;
+
     for(int i=0; i<n; ++i) {
         if(nodosrestantes[i]){
-            int n2=aristas[i].size();
+            int n2=a.adjList[i].size();
             for(int j=0; j<n2 ; ++j){
-                int conexion=aristas[i][j];
-                if( nodosrestantes[conexion]) m++;
-
-            }
-        }
-
-    }
-
-    cout<<nf<<" "<<m<<endl;
-    for(int i=0; i<n; ++i) {
-        if(nodosrestantes[i]){
-            int n2=aristas[i].size();
-            for(int j=0; j<n2 ; ++j){
-                int conexion=aristas[i][j];
-                if(nodosrestantes[conexion]){
-                    cout<<nodosfinales[i]<<" "<<nodosfinales[conexion]<<endl;
+                int conexion=a.adjList[i][j];
+                if( nodosrestantes[conexion]) {
+                    inducido.adjList[nodosfinales[i]].push_back(nodosfinales[conexion]);
+                  //  cout<<nodosfinales[i]<<" "<<nodosfinales[conexion]<<endl;
                 }
 
             }
         }
 
     }
+    return inducido.connected_components();
 
-}
+
+    }
